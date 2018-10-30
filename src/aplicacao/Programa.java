b79c90b8690e5c8353caaece200c4232dfba1912
danilovalerio <danilo_vs@hotmail.com>
2018-10-30 15:33:15 -0300

    Exemplo de uma lista curinga* (Obs. Não podemos adicionar item numa lista curinga)

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,9 +1,27 @@
 package aplicacao;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Programa {
 
 	public static void main(String[] args) {
+		List<Integer> myInts = Arrays.asList(5, 2, 10);
+		printList(myInts);
 		
+		List<String> myStrings = Arrays.asList("João", "Maria", "Antonio");
+		printList(myStrings);
+	}
+	
+	/* 
+	 * ? (interrogacão) é quem determina o tipo curinga
+	 * Veja no método printList abaixo que com o mesmo podemos
+	 * imprimir qualquer tipo de lista 
+	 */
+	public static void printList(List<?> list) {
+		for (Object obj : list) {
+			System.out.println(obj);
+		}
 
 	}
 
